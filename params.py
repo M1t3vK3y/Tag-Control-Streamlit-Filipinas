3dfea1c28d02f971e395db821703e54f6ba2388a
Kevin U. Antela <145849005+M1t3vK3y@users.noreply.github.com>
2024-08-14 11:17:16 +0200

    Update params.py

--- params.py
+++ params.py
@@ -4,6 +4,7 @@ import random
 import plotly.graph_objects as go
 import logging
 
+#secrets
 urls = [
     (st.secrets["URLS"]["URL1"],st.secrets["KEYS"]["KEY1"],st.secrets["NAMES"]["NAME1"]),
         (st.secrets["URLS"]["URL2"],st.secrets["KEYS"]["KEY2"],st.secrets["NAMES"]["NAME2"]),
